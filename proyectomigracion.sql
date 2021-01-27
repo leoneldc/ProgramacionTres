5c27f43acddc31eefc98a793b93e921b51e4f525
jeffersondavila <jeffersondavila16@gmail.com>
2021-01-26 19:45:58 -0600

    foreign key en citas y en agendar

--- proyectomigracion.sql
+++ proyectomigracion.sql
@@ -2,10 +2,20 @@ create database ProyectoMigracion;
 use ProyectoMigracion;
 
 create table banco(
-noBoleta int(10) primary key not null,
+noBoleta int(10) not null,
 noComprobante int(10) not null,
-estado varchar(1)not null
+estado varchar(1)not null,
+primary key (noBoleta, noComprobante)
 )ENGINE=INNODB default CHARSET=LATIN1;
+
+CREATE TABLE Renap (
+  DPI int(13) primary key not null,
+ NombreC varchar(50) not null,
+ FechaDnacimiento varchar(30)not null,
+ nacionalidad varchar(45) not null,
+ EstadoCiil varchar(45) not null,
+ sexo varchar(25) not null
+) ENGINE=INNODB default CHARSET=LATIN1;
 
 create table datospersonales(
 noIdentificacion int(14) primary key not null,
@@ -32,30 +42,23 @@ foreign key (noIdentificacion) references datospersonales(noIdentificacion)
 )ENGINE=INNODB default CHARSET=LATIN1;
 
 CREATE TABLE citas (
-  id_citas int(11) not null auto_increment,
-  recibo_citas varchar(50) not null,
-  boleta_citas varchar(50) not null,
+  id_citas int(20) primary key auto_increment not null,
+  boleta_citas int(10) not null,
+  recibo_citas int(10) not null,
   nombre_citas varchar(50) not null,
   apellido_citas varchar(50) not null,
-  dpi_citas varchar(50) not null,
-  primary key (id_citas)
+  dpi_citas int(50) not null,
+  foreign key (dpi_citas) references Renap(DPI),
+  foreign key (boleta_citas, recibo_citas) references banco(noBoleta, noComprobante)
 ) ENGINE=INNODB default CHARSET=LATIN1;
 
 CREATE TABLE agendar (
-  id_agendar int(11) not null auto_increment,
+  id_agendar int(20) auto_increment primary key not null,
   pais_agendar varchar(50) not null,
   departamento_agendar varchar(50) not null,
   municipio_agendar varchar(50) not null,
   centro_agendar varchar(50) not null,
   fecha varchar(50) not null,
   horario varchar(50) not null,
-  primary key (id_agendar)
-) ENGINE=INNODB default CHARSET=LATIN1;
-CREATE TABLE Renap (
-  DPI int(13) primary key not null,
- NombreC varchar(50) not null,
- FechaDnacimiento varchar(30)not null,
- nacionalidad varchar(45) not null,
- EstadoCiil varchar(45) not null,
- sexo varchar(25) not null
+  foreign key (id_agendar) references citas(id_citas)
 ) ENGINE=INNODB default CHARSET=LATIN1;
